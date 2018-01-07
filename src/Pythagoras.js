da15af585fe109fea650dc01c33b1724f56ad974
Swizec Teller <swizec@swizec.com>
2018-01-07 15:26:05 -0800

    Fix lean direction

--- src/Pythagoras.js
+++ src/Pythagoras.js
@@ -1,40 +1,50 @@
-
-import React from 'react';
-import { interpolateViridis } from 'd3-scale';
+import React from "react";
+import { interpolateViridis } from "d3-scale";
 
 Math.deg = function(radians) {
-  return radians * (180 / Math.PI);
+    return radians * (180 / Math.PI);
 };
 
-const memoizedCalc = function () {
+const memoizedCalc = (function() {
     const memo = {};
 
-    const key = ({ w, heightFactor, lean }) => [w,heightFactor, lean].join('-');
+    const key = ({ w, heightFactor, lean }) =>
+        [w, heightFactor, lean].join("-");
 
-    return (args) => {
+    return args => {
         const memoKey = key(args);
 
         if (memo[memoKey]) {
             return memo[memoKey];
-        }else{
+        } else {
             const { w, heightFactor, lean } = args;
 
-            const trigH = heightFactor*w;
+            const trigH = heightFactor * w;
 
             const result = {
-                nextRight: Math.sqrt(trigH**2 + (w * (.5+lean))**2),
-                nextLeft: Math.sqrt(trigH**2 + (w * (.5-lean))**2),
-                A: Math.deg(Math.atan(trigH / ((.5-lean) * w))),
-                B: Math.deg(Math.atan(trigH / ((.5+lean) * w)))
+                nextRight: Math.sqrt(trigH ** 2 + (w * (0.5 - lean)) ** 2),
+                nextLeft: Math.sqrt(trigH ** 2 + (w * (0.5 + lean)) ** 2),
+                A: Math.deg(Math.atan(trigH / ((0.5 + lean) * w))),
+                B: Math.deg(Math.atan(trigH / ((0.5 - lean) * w)))
             };
 
             memo[memoKey] = result;
             return result;
         }
-    }
-}();
+    };
+})();
 
-const Pythagoras = ({ w,x, y, heightFactor, lean, left, right, lvl, maxlvl }) => {
+const Pythagoras = ({
+    w,
+    x,
+    y,
+    heightFactor,
+    lean,
+    left,
+    right,
+    lvl,
+    maxlvl
+}) => {
     if (lvl >= maxlvl || w < 1) {
         return null;
     }
@@ -45,34 +55,45 @@ const Pythagoras = ({ w,x, y, heightFactor, lean, left, right, lvl, maxlvl }) =>
         lean: lean
     });
 
-    let rotate = '';
+    let rotate = "";
 
     if (left) {
         rotate = `rotate(${-A} 0 ${w})`;
-    }else if (right) {
+    } else if (right) {
         rotate = `rotate(${B} ${w} ${w})`;
     }
 
     return (
         <g transform={`translate(${x} ${y}) ${rotate}`}>
-            <rect width={w} height={w}
-                  x={0} y={0}
-                  style={{fill: interpolateViridis(lvl/maxlvl)}} />
+            <rect
+                width={w}
+                height={w}
+                x={0}
+                y={0}
+                style={{ fill: interpolateViridis(lvl / maxlvl) }}
+            />
 
-            <Pythagoras w={nextLeft}
-                        x={0} y={-nextLeft}
-                        lvl={lvl+1} maxlvl={maxlvl}
-                        heightFactor={heightFactor}
-                        lean={lean}
-                        left />
+            <Pythagoras
+                w={nextLeft}
+                x={0}
+                y={-nextLeft}
+                lvl={lvl + 1}
+                maxlvl={maxlvl}
+                heightFactor={heightFactor}
+                lean={lean}
+                left
+            />
 
-            <Pythagoras w={nextRight}
-                        x={w-nextRight} y={-nextRight}
-                        lvl={lvl+1} maxlvl={maxlvl}
-                        heightFactor={heightFactor}
-                        lean={lean}
-                        right />
-
+            <Pythagoras
+                w={nextRight}
+                x={w - nextRight}
+                y={-nextRight}
+                lvl={lvl + 1}
+                maxlvl={maxlvl}
+                heightFactor={heightFactor}
+                lean={lean}
+                right
+            />
         </g>
     );
 };
