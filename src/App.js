17d93b36ba5bd4c46c72a4a2e9b80f916747c275
Swizec Teller <swizec@swizec.com>
2021-06-15 08:12:47 -0700

    Make startTransition optional

--- src/App.js
+++ src/App.js
@@ -1,97 +1,127 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 import Pythagoras from "./Pythagoras";
 
-class App extends Component {
-    svg = {
+function App() {
+    const svg = {
         width: 1280,
         height: 600,
     };
-    state = {
-        treeSize: 4,
-        baseW: 80,
-        heightFactor: 0.4,
-        lean: 0,
-    };
-    running = false;
-    maxTreeSize = 11;
+    const baseWidth = 80;
+    const heightFactor = 0.4;
 
-    changeTreeSize(event) {
-        React.startTransition(() => {
-            this.setState({
-                treeSize: Number(event.target.value),
+    const [treeSize, setTreeSize] = useState(6);
+    const [treeLean, setTreeLean] = useState(0);
+    const [enableStartTransition, setEnableStartTransition] = useState(false);
+
+    // grow/shrink the tree, possibly using startTransition
+    function changeTreeSize(event) {
+        if (enableStartTransition) {
+            React.startTransition(() => {
+                setTreeSize(Number(event.target.value));
             });
-        });
+        } else {
+            setTreeSize(Number(event.target.value));
+        }
     }
 
-    changeTreeLean(event) {
-        React.startTransition(() => {
-            this.setState({
-                lean: Number(event.target.value),
+    // lean the tree, possibly using startTransition
+    function changeTreeLean(event) {
+        if (enableStartTransition) {
+            React.startTransition(() => {
+                setTreeLean(Number(event.target.value));
             });
-        });
+        } else {
+            setTreeLean(Number(event.target.value));
+        }
     }
 
-    render() {
-        return (
-            <div className="App">
-                <div className="App-header" style={{ marginBottom: "1rem" }}>
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>This is a dancing Pythagoras tree</h2>
+    function toggleStartTransition() {
+        setEnableStartTransition(!enableStartTransition);
+    }
+
+    return (
+        <div className="App">
+            <div className="App-header" style={{ marginBottom: "1rem" }}>
+                <img src={logo} className="App-logo" alt="logo" />
+                <h2>This is a leaning Pythagoras tree</h2>
+            </div>
+
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                }}
+            >
+                <div>
+                    <label>Use startTransition</label>
+                    <br />
+                    <input
+                        type="checkbox"
+                        selected={enableStartTransition}
+                        onChange={toggleStartTransition}
+                    />
                 </div>
-                <label>Lean the tree:</label>
-                <br />
-                <input
-                    type="range"
-                    value={this.state.lean}
-                    onChange={(event) => this.changeTreeLean(event)}
-                    min="-0.5"
-                    max="0.5"
-                    step="0.05"
-                    style={{ width: this.svg.width / 3 }}
-                />
-                <div style={{ display: "flex", flexDirection: "row" }}>
-                    <div style={{ display: "flex", flexDirection: "column" }}>
-                        <label>Grow the tree</label>
-                        <input
-                            type="range"
-                            value={this.state.treeSize}
-                            onChange={(event) => this.changeTreeSize(event)}
-                            min="0"
-                            max={this.maxTreeSize}
-                            step="1"
-                            style={{
-                                transform: `rotate(-90deg) translate(-${
-                                    this.svg.height / 2
-                                }px, 0)`,
-                                width: this.svg.height / 2,
-                            }}
-                        />
-                    </div>
-
-                    <svg
-                        width={this.svg.width}
-                        height={this.svg.height}
-                        ref="svg"
-                        style={{ border: "1px solid lightgray" }}
-                    >
-                        <Pythagoras
-                            w={this.state.baseW}
-                            h={this.state.baseW}
-                            heightFactor={this.state.heightFactor}
-                            lean={-this.state.lean}
-                            x={this.svg.width / 2 - 40}
-                            y={this.svg.height - this.state.baseW}
-                            lvl={0}
-                            maxlvl={this.state.treeSize}
-                        />
-                    </svg>
+                <div>
+                    <label>Lean the tree:</label>
+                    <br />
+                    <input
+                        type="range"
+                        value={treeLean}
+                        onChange={changeTreeLean}
+                        min="-0.5"
+                        max="0.5"
+                        step="0.05"
+                        style={{ width: svg.width / 3 }}
+                    />
                 </div>
             </div>
-        );
-    }
+
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                    <label>
+                        Grow the tree
+                        <br />
+                        Bigger is slower
+                    </label>
+                    <input
+                        type="range"
+                        value={treeSize}
+                        onChange={changeTreeSize}
+                        min="0"
+                        max="11"
+                        step="1"
+                        style={{
+                            transform: `rotate(-90deg) translate(-${
+                                svg.height / 2
+                            }px, 0)`,
+                            width: svg.height / 2,
+                        }}
+                    />
+                </div>
+
+                <svg
+                    width={svg.width}
+                    height={svg.height}
+                    style={{ border: "1px solid lightgray" }}
+                >
+                    <Pythagoras
+                        w={baseWidth}
+                        h={baseWidth}
+                        heightFactor={heightFactor}
+                        lean={-treeLean}
+                        x={svg.width / 2 - 40}
+                        y={svg.height - baseWidth}
+                        lvl={0}
+                        maxlvl={treeSize}
+                    />
+                </svg>
+            </div>
+        </div>
+    );
 }
 
 export default App;
