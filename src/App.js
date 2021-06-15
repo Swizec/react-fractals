f0a0c365cd0fb28f34fc853cd7883de3169c7d44
Dan Abramov <dan.abramov@me.com>
2021-06-15 18:36:43 +0100

    Add pending state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useTransition } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -20,6 +20,7 @@ function App() {
 
     const [treeLeanInput, setTreeLeanInput] = useState(0);
     const [treeLean, setTreeLean] = useState(0);
+    const [isLeaning, startLeaning] = useTransition()
 
     const [enableStartTransition, setEnableStartTransition] = useState(false);
 
@@ -43,7 +44,7 @@ function App() {
 
         // update visuals
         if (enableStartTransition) {
-            React.startTransition(() => {
+            startLeaning(() => {
                 setTreeLean(value);
             });
         } else {
@@ -119,7 +120,10 @@ function App() {
                 <svg
                     width={svg.width}
                     height={svg.height}
-                    style={{ border: "1px solid lightgray" }}
+                    className={isLeaning ? 'pending' : 'done'}
+                    style={{
+                        border: "1px solid lightgray",
+                    }}
                 >
                     <Pythagoras
                         w={baseWidth}
